8ba4a8e041d71220d0fc435bbdb0adf1ec9ce75d
David <usadavew@gmail.com>
2026-07-17 06:34:39 -0400

    New sw.js

--- www/sw.js
+++ www/sw.js
@@ -1,1 +1,6 @@
 //Service Worker
+// This tells Android the service worker is active and listening
+self.addEventListener('fetch', (event) => {
+    // The app will fetch assets normally from the web
+    event.respondWith(fetch(event.request));
+});
